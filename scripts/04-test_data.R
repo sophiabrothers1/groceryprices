61b6aae2d0b2d067e89c887872b0c8c7a8f0711d
sophiabrothers1 <107134583+sophiabrothers1@users.noreply.github.com>
2024-11-13 22:57:06 -0500

    data tests

--- scripts/04-test_data.R
+++ scripts/04-test_data.R
@@ -1,1 +1,34 @@
-# test for analysis data
+# Load libraries
+library(DBI)
+library(RSQLite)
+library(here)
+library(testthat)
+
+# Define needed paths
+db_path <- here("data", "raw_data", "hammer-2-processed.sqlite")
+sql_file_path <- here("scripts", "02-query.sql")
+
+# Test that the SQL query runs
+test_that("SQL query returns expected results", {
+  # Connect to the SQLite database
+  con <- dbConnect(SQLite(), dbname = db_path)
+  
+  # Load query from file
+  sql_query <- paste(suppressWarnings(readLines(sql_file_path)), collapse = " ")
+  
+  # Execute the query
+  query_result <- dbGetQuery(con, sql_query)
+  
+  # Disconnect from database
+  dbDisconnect(con)
+  
+  # Check that query_result is a data frame
+  expect_s3_class(query_result, "data.frame")
+  
+  # Check that the result has expected columns
+  expect_true(all(c("vendor", "most_expensive_count") %in% colnames(query_result)))
+  
+  # Check that there are rows in the result
+  expect_gt(nrow(query_result), 0)
+  
+})
